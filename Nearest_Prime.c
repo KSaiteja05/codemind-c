51b0f74fa754545d71da9b513e6c73a3bea3555f
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-10-16 12:44:58 +0530

    #include<stdio.h> int isprime(int n) {     if(n<=1) return 0;     for(int i=2;i<n;i++)     {         if(n%i==0) return 0;     }     return 1; } int main() {     int t;     scanf("%d",&t);     for(int i=0;i<t;i++)     {         int n;         scanf("%d",&n);         int m=n,x,y, q=n;         while(n)         {             if(isprime(n)==1)             {                 x=n;                 break;             }             else             {                 n+=1;             }         }         while(m)         {             if(isprime(m)==1)             {                 y=m;                 break;             }             else             {                 m-=1;             }         }         if(x-q<q-y)         {             printf("%d ",x);         }         else         {             printf("%d ",y);         }     } }

---
+++ Nearest_Prime.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int isprime(int n)
+{
+    if(n<=1) return 0;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++)
+    {
+        int n;
+        scanf("%d",&n);
+        int m=n,x,y, q=n;
+        while(n)
+        {
+            if(isprime(n)==1)
+            {
+                x=n;
+                break;
+            }
+            else
+            {
+                n+=1;
+            }
+        }
+        while(m)
+        {
+            if(isprime(m)==1)
+            {
+                y=m;
+                break;
+            }
+            else
+            {
+                m-=1;
+            }
+        }
+        if(x-q<q-y)
+        {
+            printf("%d
+",x);
+        }
+        else
+        {
+            printf("%d
+",y);
+        }
+    }
+}
